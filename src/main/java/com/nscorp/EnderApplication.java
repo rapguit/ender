4babd7cbb04f5d184aa1d09f15dd8b094efe6431
Raphael Fialho <rapguit@gmail.com>
2017-03-23 01:02:25 -0300

    esqueleto do projeto

---
+++ src/main/java/com/nscorp/EnderApplication.java
@@ -0,0 +1,12 @@
+package com.nscorp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnderApplication.class, args);
+	}
+}
