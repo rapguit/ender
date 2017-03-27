e36ed1beeeeab4d10190284f3d2553829d760b7c
Raphael Fialho <rapguit@gmail.com>
2017-03-27 04:09:36 -0300

    front controller e paginas para client web

---
+++ src/main/java/com/nscorp/address/controller/FrontController.java
@@ -0,0 +1,82 @@
+package com.nscorp.address.controller;
+
+import com.nscorp.model.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import javax.servlet.ServletRequest;
+import javax.validation.Valid;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+/**
+ * Created by raphael on 27/03/17.
+ */
+@Controller
+@RequestMapping("/ender/address")
+public class FrontController {
+
+    @Autowired
+    private RestTemplate consumer;
+
+    @GetMapping("list")
+    public String getAddressList(Model model, ServletRequest req){
+        String url = String.format("http://%s:%s/ender/ws/address", req.getServerName(), req.getServerPort());
+        List<Address> list = asList(consumer.getForObject(url, Address[].class));
+        model.addAttribute("addressList", list);
+
+        return "list";
+    }
+
+    @GetMapping("new")
+    public String newAddress(Address address){
+        return "new";
+    }
+
+    @GetMapping("view/{id}")
+    public String viewAddress(@PathVariable String id, Model model, ServletRequest req){
+        String url = String.format("http://%s:%s/ender/ws/address/%s", req.getServerName(), req.getServerPort(), id);
+        Address address = consumer.getForObject(url, Address.class);
+        model.addAttribute("address", address);
+
+        return "view";
+    }
+
+    @GetMapping("edit/{id}")
+    public String editAddress(@PathVariable String id, Model model, ServletRequest req){
+        String url = String.format("http://%s:%s/ender/ws/address/%s", req.getServerName(), req.getServerPort(), id);
+        Address address = consumer.getForObject(url, Address.class);
+        model.addAttribute("address", address);
+
+        return "edit";
+    }
+
+    @GetMapping("remove/{id}")
+    public String removeAddress(@PathVariable String id, ServletRequest req){
+        String url = String.format("http://%s:%s/ender/ws/address/%s", req.getServerName(), req.getServerPort(), id);
+        consumer.delete(url);
+
+        return "redirect:../list";
+    }
+
+    @PostMapping("save")
+    public String saveAddress(@Valid Address address, ServletRequest req, BindingResult result){
+        if (result.hasErrors()){
+            return "new";
+        }
+
+        String url = String.format("http://%s:%s/ender/ws/address", req.getServerName(), req.getServerPort());
+        consumer.postForObject(url, address, Address.class);
+
+        return "view";
+    }
+
+}
