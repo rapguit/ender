11bee70fed2e6ce43e8b553ecfb3b3cbaba9ca13
Raphael Fialho <rapguit@gmail.com>
2017-03-29 01:42:30 -0300

    ajuste para deploy em container

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,3 @@ COPY target/ender-*.war webapps/ROOT.war
 #ENV config
 ENV TZ=America/Sao_Paulo
 RUN ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
-
-CMD ["catalina.sh", "run"]
--- src/main/java/com/nscorp/EnderApplication.java
+++ src/main/java/com/nscorp/EnderApplication.java
@@ -2,11 +2,18 @@ package com.nscorp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-public class EnderApplication {
+public class EnderApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EnderApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnderApplication.class, args);
